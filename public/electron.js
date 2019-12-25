9e5629a80bf047b825e649ce1d0a857741420e8c
Giovanni <531406@student.glu.nl>
2019-12-25 17:28:54 +0100

    nani?

---
+++ public/electron.js
@@ -0,0 +1,68 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+require('v8-compile-cache')
+let mainWindow,
+    loadingScreen,
+    windowParams = {
+        fullscreen: false,
+        frame: false,
+        show: false
+    };
+// where public folder on the root dir
+
+
+function createWindow() {
+    mainWindow = new BrowserWindow(windowParams);
+
+
+    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.on("closed", () => (mainWindow = null));
+    // Open the DevTools.
+
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.show();
+
+        if (loadingScreen) {
+            let loadingScreenBounds = loadingScreen.getBounds();
+            mainWindow.setBounds(loadingScreenBounds);
+            loadingScreen.close();
+        }
+    });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    })
+
+}
+
+function createLoadingScreen() {
+    loadingScreen = new BrowserWindow(Object.assign(windowParams, {
+        parent: mainWindow
+    }));
+    loadingScreen.on('closed', () => loadingScreen = null);
+    loadingScreen.webContents.on('did-finish-load', () => {
+        loadingScreen.show();
+    });
+}
+app.on('ready', () => {
+    createLoadingScreen();
+    createWindow();
+});
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
