bb0bc236ce4f075dab1912315aee8a7fb46a1296
Giovanni <531406@student.glu.nl>
2019-12-26 15:29:59 +0100

    added a temporary signout button for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react'
+import firebase from './firebase'
+import {Button} from 'semantic-ui-react'
 
 class App extends Component {
+  state ={
+    user: this.props.currentUser,
+    storageRef: firebase.storage().ref(),
+    userRef: firebase.auth().currentUser,
+    usersRef: firebase.database().ref("users")
+  }
+
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("signed out"))
+  }
+
   render(){ 
     return(
       <div className="App">
-        <h1>Hallo</h1>
+        <Button onClick={this.handleSignout}>Sign out</Button>
       </div>
     )
   }
