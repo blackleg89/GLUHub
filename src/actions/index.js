55ab4e6a7e4174b9bae552b590778e50476c1d16
Giovanni <531406@student.glu.nl>
2019-12-31 20:41:27 +0100

    idk anymore

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,21 +14,3 @@ export const clearUser = () =>{
         type: actionTypes.CLEAR_USER
     }
 }
-
-export const setCurrentChannel = channel =>{
-    return{
-        type:actionTypes.SET_CURRENT_CHANNEL, 
-        payload: {
-            currentChannel: channel
-        }
-    }
-}
-
-export const setUserPosts = userPosts =>{
-    return{
-        type: actionTypes.SET_USER_POSTS,
-        payload:{
-            userPosts
-        }
-    }
-}
