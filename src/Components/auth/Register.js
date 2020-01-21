cb79f64f680abb12dfb2b88c0b9acc6d180baee5
gio <531406@student.glu.nl>
2020-01-21 09:55:21 +0100

    fixed login link

--- src/Components/auth/Register.js
+++ src/Components/auth/Register.js
@@ -197,7 +197,7 @@ class Register extends Component {
                         </Message>
                     )}
                     <Message>
-                        Already have an acount? <Link to="/register">Login</Link>
+                        Already have an acount? <Link to="/login">Login</Link>
                     </Message>
                 </Grid.Column>  
             </Grid>
