cde71b0c468c5dda6db1d9d50be5ea3f861025c9
Gio <531406@student.glu.nl>
2020-02-14 18:26:42 +0100

    added fetched results to a segment

--- src/Components/Git/Components/Views/Test.js
+++ src/Components/Git/Components/Views/Test.js
@@ -2,33 +2,34 @@ import React, { Component } from 'react';
 import firebase from '../../../../firebase'
 import axios from 'axios'
 // import SearchForm from '../Data/SearchForm';
-// import RepoList from '../Data/RepoList';
-import {Button, Grid, Label, Header} from 'semantic-ui-react'
+import RepoList from '../Data/RepoList';
+import {Button, Grid, Label, Header, Segment} from 'semantic-ui-react'
 export default class Test extends Component{
-        state = {
+    constructor(){
+        super()
+        this.state = {
         repos: [],
         loading: true,
         user: firebase.auth().currentUser,
       };
-
+    }
       componentDidMount() {
-        this.fetchRepos();
+        this.performSearch();
       }
 
-n     
-      fetchRepos = () =>{
-        let userName = this.state.user.displayName
-        axios.get(`https://api.github.com/users/${userName}/repos`)
-        .then(response =>{
-          this.setState({
-            repos: response.data.items,
-            loading:false
+    
+      performSearch = (query = 'phaser') => {
+        axios.get(`https://api.github.com/search/repositories?q=${query}`)
+          .then(response => {
+            this.setState({
+              query: query,
+              repos: response.data.items,
+              loading: false
+            });
           })
-          console.log(response)
-        })
-        .catch(error =>{
-          console.error(error)
-        })
+          .catch(error => {
+            console.log('Error fetching and parsing data', error);
+          });    
       }
 
     render(){
@@ -37,21 +38,17 @@ n
         repos
       } = this.state
         return (
-            <Grid style={{background:"#000"}}>
-              <Grid.Column>
-                <Grid.Row centered style={{margin:0}}>
-                  <Header inverted as="h2">
-                    <Header.Content>{user.displayName}</Header.Content>
-                  </Header>
-                  <Button onClick={this.fetchRepos}/>
-                </Grid.Row>
-              </Grid.Column>
-              <Grid.Column>
-                  <Grid.Row centered>
-                    {repos.name}
-                  </Grid.Row>
-              </Grid.Column>
-            </Grid>
+          <Segment>
+            <div>
+              <div className="main-content">
+                {
+                  (this.state.loading)
+                  ? <p>Loading...</p>
+                  : <div><h2>{this.state.query}</h2><RepoList data={this.state.repos} /></div>
+                }          
+              </div>
+          </div>
+        </Segment>
           );
     }
 }
